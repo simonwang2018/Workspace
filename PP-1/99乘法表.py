1165838e960b66c67d6beeae18789c2edd559fe3
simonwang2018 <wahaition@126.com>
2020-10-21 17:29:25 +0800

    10月21号新增12

--- PP-1/99乘法表.py
+++ PP-1/99乘法表.py
@@ -3,6 +3,6 @@ for i in range(1,10):
     for m in range (1,i+1):
         sum = i * m
         if m < i:
-            print(m,'x',i,  '=', sum ,end= '   ')
+            print(m,'x',i,'=',sum ,end= '   ')
         else:
-            print(m,'x',i, '=', sum)
+            print(m,'x',i,'=',sum)
